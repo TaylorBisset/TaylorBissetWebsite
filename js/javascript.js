8da43dae350d91ff7862e8d869b10defb647862b
Taylor Bisset <87632154+TaylorBisset@users.noreply.github.com>
2024-01-31 10:17:34 -0800

    Modified styles

--- js/javascript.js
+++
@@ -1,5 +0,0 @@
-// Get the current year
-const currentYear = new Date().getFullYear();
-
-// Update the copyright text in footer
-document.getElementById('copyright').textContent = `&copy; ${currentYear} TaylorBisset.com`;
